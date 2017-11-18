f2c881b0cab1de4f84c68e49baedf19bdfb987b3
Ujjwal Krishnamurthi <texanrohan1@gmail.com>
2017-11-17 20:50:20 -0800

    initial commit

---
+++ Think Before You Talk/Think Before You TalkTests/Think_Before_You_TalkTests.swift
@@ -0,0 +1,36 @@
+//
+//  Think_Before_You_TalkTests.swift
+//  Think Before You TalkTests
+//
+//  Created by Rohan Rao on 11/17/17.
+//  Copyright © 2017 nFinity Labs. All rights reserved.
+//
+
+import XCTest
+@testable import Think_Before_You_Talk
+
+class Think_Before_You_TalkTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
